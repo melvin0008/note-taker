464f21d1573242820f82a81db8d74e39a193056f
Melvin <melvin0008@gmail.com>
2015-07-03 19:03:34 +0530

    added basic profile component

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,14 +1,14 @@
 var React = require('react'),
     Main = require('../components/Main'),
     Home = require('../components/Home'),
+    Profile =  require('../components/Profile'),
     Router = require('react-router'),
     DefaultRoute = Router.DefaultRoute,
     Route = Router.Route;
 
 module.exports= (
 	<Route name="app" path='/' handler={Main}>
-
-
+		<Route name="profile" path="profile/:username" handler={Profile}/>
 		<DefaultRoute handler={Home} />
 	</Route>
 );
