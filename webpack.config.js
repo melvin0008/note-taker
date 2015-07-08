a7747708e9c96509ba30f75efe7e0bbb3a347bf0
Melvin <melvin0008@gmail.com>
2015-07-08 23:00:25 +0530

    changed gh to deal  with socket errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,11 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports={
-devtool: 'eval',
-	entry: ['webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
-		  'webpack/hot/only-dev-server',
-		  "./app/App.js"],
+	entry: ["./app/App.js"],
 	output:{
 		path: path.join(__dirname, 'public'),
 		filename:"bundle.js",
-		publicPath: 'http://localhost:3000/public/'
 	},
-	plugins: [
-	    new webpack.HotModuleReplacementPlugin(),
-	    new webpack.NoErrorsPlugin()
-  	],
 	module:{
 		loaders:[{
 			test: /\.jsx?$/,
